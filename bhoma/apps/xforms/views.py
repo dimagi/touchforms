a53f6719adf25f8deed7594a803f3e0e2ab4e4c7
Cory Zue <czue@dimagi.com>
2010-06-23 18:23:11 -0400

    Adding callback functionality to the xform player, which allows you to call it and get it to call back to you after it's completed.

--- bhoma/apps/xforms/views.py
+++ bhoma/apps/xforms/views.py
@@ -11,11 +11,26 @@ from bhoma.apps.xforms.models.couch import CXFormInstance
 import logging
 
 
-def play(request, xform_id):
-    """Play an XForm"""
+def play(request, xform_id, callback=None):
+    """
+    Play an XForm.
+    
+    If you specify callback, instead of returning a response this view
+    will call back to your method upon completion (POST).  This allows
+    you to call the view from your own view, but specify a callback
+    method afterwards to do custom processing and responding.
+    
+    The callback method should have the following signature:
+        response = <method>(xform, document)
+    where:
+        xform = the django model of the form 
+        document = the couch object created by the instance data
+        response = a valid http response
+    """
     xform = get_object_or_404(XForm, id=xform_id)
     redirect_url = request.GET.get('redirect_url', '')
     if request.POST:
+        
         # get the instance
         instance = request.POST["output"]
         # post to couch
@@ -31,12 +46,12 @@ def play(request, xform_id):
                     doc["#%s" % key] = value
             CXFormInstance.get_db().save_doc(doc)
             
+            # CZUE: abandoning callbacks for now so commenting this out...
             # make the callbacks
-            for callback in xform.callbacks.all():
-                callback.call(doc)
-                
-            if redirect_url:
-                return HttpResponseRedirect(redirect_url)
+            # for callback in xform.callbacks.all():
+            #    callback.call(doc)
+            if callback:
+                return callback(xform, doc)
             else:
                 return HttpResponseRedirect(reverse("xform_list"))
         else:
@@ -45,7 +60,7 @@ def play(request, xform_id):
         # process post event hooks... how?
     return render_to_response(request, "xforms/xform_player.html",
                               {"xform": xform })
-                               
+                                    
 def player_proxy(request):
     """Proxy to an xform player, to avoid cross-site scripting issues"""
     data = request.raw_post_data if request.POST else None
