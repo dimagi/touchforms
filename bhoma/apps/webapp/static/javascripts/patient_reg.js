de4d20cb9801a55c8d1594dfedb6e3c54acc5e6e
Drew Roos <droos@dimagi.com>
2010-07-15 17:48:19 -0400

    finish reworking patient reg flow

--- bhoma/apps/webapp/static/javascripts/patient_reg.js
+++ bhoma/apps/webapp/static/javascripts/patient_reg.js
@@ -11,9 +11,104 @@ function wfGetPatient () {
     var qr_lookup_pat = new wfQuery(function () { return lookup(patient_id); });
     
     if (is_reg_form) {
+      var new_patient = true;
+      var id_accepted = false;
+      var registration_filled_out = false;
+      while (!id_accepted) {
+        yield q_pat_id;
+        var patient_id = q_pat_id.value;
+        
+        yield qr_lookup_pat;
+        var records_for_id = qr_lookup_pat.value;
+        
+        if (!registration_filled_out) {
+          var patient_rec = {} //new Patient();
+          for (var q in ask_patient_info(patient_rec, true)) {
+            yield q;
+          }
+          patient_rec.id = patient_id;
+          registration_filled_out = true;
+        }
+        
+        var merge_with = null;
+        if (records_for_id.length == 1) {
+          var q_correct_patient = new wfQuestion('A patient is already registered with this ID. Is this the same patient?', 'select', null, 
+                                                 ['Yes, this is the same patient',
+                                                  'No, but I entered the wrong ID',
+                                                  'No, register my new patient with this ID',
+                                                  'No, start over'], true);
+          yield q_correct_patient;
+          var corr_pat_ans = q_correct_patient.value;
 
+          if (corr_pat_ans == 4) {
+            return; //start over
+          } else if (corr_pat_ans == 2) {
+            continue; //re-enter id
+          } else if (corr_pat_ans == 1) {
+            merge_with = records_for_id[0]['uuid'];
+          }
+        } else if (records_for_id.length > 1) {
+          var q_choose_patient = qChooseAmongstPatients(records_for_id, 'Multiple patients found with that ID! Are any the same patient as the one you just registered?',
+                                                        'None of these is the same patient');
+          var chosen = false;
+          while (!chosen) {
+            yield q_choose_patient;
+            var choose_pat_ans = q_choose_patient.value;
+            if (choose_pat_ans < q_choose_patient.choices.length) {
+              var q_correct_patient = new wfQuestion('Is this the correct patient?', 'select', null, 
+                                                     ['Yes',
+                                                      'No, back to list'], true);
+              yield q_correct_patient;
+              var corr_pat_ans = q_correct_patient.value;
+              if (corr_pat_ans == 1) {
+                merge_with = records_for_id[corr_pat_ans - 1]['uuid'];
+                chosen = true;
+              }
+            } else {
+              chosen = true;
+            }
+          }
+          
+          if (merge_with == null) {
+            var q_not_found = new wfQuestion('New registration for ID already in use: ' + patient_id, 'select', null,
+                                             ['Register this new patient with the same ID',
+                                              'I entered the wrong ID',
+                                              'Start over'], true);
+            yield q_not_found;
+            var not_found_ans = q_not_found.value;
+            if (not_found_ans == 3) {
+              return; //start over
+            } else if (not_found_ans == 2) {
+              continue; //re-enter id
+            }
+          }
+        }  
+        id_accepted = true;
+      }
 
+      if (merge_with == null) {
+        var qr_dup_check = new wfQuery(function () { return fuzzy_match(patient_rec); });
+        yield qr_dup_check;
+        var candidate_duplicate = qr_dup_check.value;
+
+        if (candidate_duplicate != null) {
+          var q_merge_dup = new wfQuestion('Similar patient found! Is this the same patient?', 'select', null, 
+                                           ['Yes, these are the same person',
+                                            'No, this is a different person'], true);
+          yield q_merge_dup;
+          merge = (q_merge_dup.value == 1);
+          if (merge) {
+            merge_with = candidate_duplicate['uuid'];
+            yield new wfAlert('Remember to merge the two paper records for this patient');
+          }
+        }
+      }
+
+      if (merge_with) {
+        data['merge_with'] = merge_with;
+      }
     } else {
+
       var id_accepted = false;
       while (!id_accepted) {
         yield q_pat_id;
@@ -53,16 +148,8 @@ function wfGetPatient () {
             patient_rec = records_for_id[0];
           }
         } else {
-          //// make me a function
-          var choices = [];
-          for (patrec in Iterator(records_for_id)) {
-            patrec = patrec[1];
-            choices.push(patrec['fname'] + " " + patrec['lname'] + " " + Math.floor((new Date() - new Date(patrec['dob']))/(1000.*86400*365.2425)) + "/" + patrec['sex'].toUpperCase());
-          }
-          choices.push('None of these is the correct patient');
-          ////
-
-          var q_choose_patient = new wfQuestion('Multiple patients found with that ID! Choose the correct patient', 'select', null, choices, true);
+          var q_choose_patient = qChooseAmongstPatients(records_for_id, 'Multiple patients found with that ID! Choose the correct patient',
+                                                        'None of these is the correct patient');
           var chosen = false;
           while (!chosen) {
             yield q_choose_patient;
@@ -131,136 +218,11 @@ function wfGetPatient () {
       } else {
         new_patient = false;
       }
+
     }
 
     data['patient'] = patient_rec;
     data['new'] = new_patient;
-
-
-
-
-    /*
-
-
-
-    while (true) {
-
-
-
-      var qr_lookup_pat = new wfQuery(function () { return lookup(patient_id); });
-      yield qr_lookup_pat;
-      var patient_rec = qr_lookup_pat.value;
-
-      if (patient_rec == null) {
-      
-        var q_reg_new = new wfQuestion('Not found. Register new patient?', 'select', null, 
-                                       ['Yes, I have a registration form',
-                                        'Yes, but I don\'t have a registration form',
-                                        'No, start over'], true);
-        yield q_reg_new;
-        var reg_new_ans = q_reg_new.value;
-        var reg_new = (reg_new_ans == 1 || reg_new_ans == 2);
-        var has_reg_form = (reg_new_ans == 1);
-      
-        if (reg_new) {
-        
-          var patient_rec = {} //new Patient();
-          for (var q in ask_patient_info(patient_rec, has_reg_form)) {
-            yield q;
-          }
-          patient_rec.id = patient_id;
-        
-          var qr_dup_check = new wfQuery(function () { return fuzzy_match(patient_rec); });
-          yield qr_dup_check;
-          var candidate_duplicate = qr_dup_check.value;
-
-          if (candidate_duplicate != null) {
-            var q_merge_dup = new wfQuestion('Similar patient found! Is this the same patient?', 'select', null, 
-                                             ['Yes, these are the same person',
-                                              'No, this is a different person'], true);
-            yield q_merge_dup;
-            merge = (q_merge_dup.value == 1);
-          } else {
-            merge = false;
-          }
-        
-          data['new_patient'] = patient_rec;
-          if (merge) {
-            data['merge_with'] = candidate_duplicate.id;
-            yield new wfAlert('Remember to merge the two paper records for this patient');
-          }
-        
-          done = true;
-        } else {
-          // fall through; go back to 'pat id' question
-        }
-      
-      } else {
-
-        q_correct_patient = new wfQuestion('Is this the correct patient?', 'select', null,
-                                           ['Yes, this is the patient',
-                                            'No, this is the wrong patient, but right ID',
-                                            'No, I mistyped the patient ID'], true);
-        yield q_correct_patient;                                                             
-        correct_pat = (q_correct_patient.value == 1);
-        correct_id_wrong_pat = (q_correct_patient.value == 2);
-
-        if (correct_pat) {
-          data['use_patient'] = patient_rec;
-          done = true;
-        } else {
-          if (correct_id_wrong_pat) {
-            q_has_reg_form = new wfQuestion('Is this a registration form?', 'select', null, ['Yes', 'No'], true);
-            yield q_has_reg_form;
-            has_reg_form = (q_has_reg_form.value == 1);
-
-            var cur_patient_rec = {} //new Patient();
-            for (var q in ask_patient_info(cur_patient_rec, has_reg_form)) {
-              yield q;
-            }
-            cur_patient_rec.id = patient_id;
-
-            qr_match = new wfQuery(function () { return fuzzy_match(cur_patient_rec); });
-            yield qr_match;
-            candidate_match = qr_match.value;
-
-            if (candidate_match != null) {
-              q_patient_instead = new wfQuestion('A similar patient was found with a different ID. Use this patient instead?', 'select', null, ['Yes', 'No'], true);
-              yield q_patient_instead;
-              used_other = (q_patient_instead.value == 1);
-            } else {
-              used_other = false;
-            }
-
-            if (used_other) {
-              data['use_patient'] = candidate_match;
-              data['incorrect_id'] = patient_id;
-
-              yield new wfAlert('Remember to fix the IDs on these forms and put them in the correct patient file');
-              done = true;
-            } else {
-              q_use_current_patient_are_you_sure = new wfQuestion('Are you sure you want to file these forms on this patient\'s record?', 'select', null, ['Yes', 'No, start over'], true);
-              yield q_use_current_patient_are_you_sure;
-              are_you_sure = (q_use_current_patient_are_you_sure.value == 1);
-
-              if (are_you_sure) {
-                data['use_patient'] = patient_rec;
-                data['conflicting_patient_info'] = cur_patient_rec;
-                data['update_info'] = has_reg_form;
-                done = true;
-              } else {
-                // fall through; go back to 'pat id' question
-              }
-            }
-          } else {
-            // fall through; go back to 'pat id' question
-          }
-        }
-      }
-    }
-
-    */
-
   }
 
   var onFinish = function (data) {
@@ -326,4 +288,15 @@ function fuzzy_match (patient_rec) {
   } else {
     return null;
   }
+}
+
+function qChooseAmongstPatients(records, qCaption, noneCaption) {
+  var choices = [];
+  for (patrec in Iterator(records)) {
+    patrec = patrec[1];
+    choices.push(patrec['fname'] + " " + patrec['lname'] + " " + Math.floor((new Date() - new Date(patrec['dob']))/(1000.*86400*365.2425)) + "/" + patrec['sex'].toUpperCase());
+  }
+  choices.push(noneCaption);
+  
+  return new wfQuestion(qCaption, 'select', null, choices, true);
 }
