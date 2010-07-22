cf87af3e9d87c53cb8beb25be8901995d6bd76b8
Cory Zue <czue@dimagi.com>
2010-07-22 16:03:17 -0400

    update metadata models to not crash when editing patients

--- bhoma/apps/xforms/models/couch.py
+++ bhoma/apps/xforms/models/couch.py
@@ -6,13 +6,17 @@ from couchdbkit.ext.django.schema import *
 import bhoma.apps.xforms.const as const
 from bhoma.utils.parsing import string_to_datetime
 import copy
+from couchdbkit.schema.properties_proxy import SchemaListProperty
+from bhoma.utils.logging import log_exception
 
 """
 Couch models.  For now, we prefix them starting with C in order to 
 differentiate them from their (to be removed) django counterparts.
 """
 
-class Metadata(Document):
+class MetadataDocument(Document):
+    # this is currently unused but may be chosen over the non-doc class
+    # if i can get nested schema props working
     """
     Metadata of an xform, from a meta block structured like:
         
@@ -37,24 +41,75 @@ class Metadata(Document):
     @classmethod
     def from_meta_block(cls, meta_block):
         instance = Metadata()
-        if const.TAG_META_CLINIC_ID in meta_block:
-            instance.clinic_id = str(meta_block[const.TAG_META_CLINIC_ID])
-        if const.TAG_META_TIMESTART in meta_block:
-            instance.time_start = string_to_datetime(meta_block[const.TAG_META_TIMESTART])
-        if const.TAG_META_TIMEEND in meta_block:
-            instance.time_end = string_to_datetime(meta_block[const.TAG_META_TIMEEND])
-        if const.TAG_META_USERNAME in meta_block:
-            instance.username = meta_block[const.TAG_META_USERNAME]
-        if const.TAG_META_USER_ID in meta_block:
-            instance.user_id = meta_block[const.TAG_META_USER_ID]
-        if const.TAG_META_UID in meta_block:
-            instance.uid = meta_block[const.TAG_META_UID]
+        try:
+            
+            if const.TAG_META_CLINIC_ID in meta_block:
+                instance.clinic_id = str(meta_block[const.TAG_META_CLINIC_ID])
+            if const.TAG_META_TIMESTART in meta_block:
+                instance.time_start = string_to_datetime(meta_block[const.TAG_META_TIMESTART])
+            if const.TAG_META_TIMEEND in meta_block:
+                instance.time_end = string_to_datetime(meta_block[const.TAG_META_TIMEEND])
+            if const.TAG_META_USERNAME in meta_block:
+                instance.username = meta_block[const.TAG_META_USERNAME]
+            if const.TAG_META_USER_ID in meta_block:
+                instance.user_id = meta_block[const.TAG_META_USER_ID]
+            if const.TAG_META_UID in meta_block:
+                instance.uid = meta_block[const.TAG_META_UID]
+        except Exception, e:
+            log_exception(e)
         return instance
     
     class Meta:
         app_label = 'xforms'
     
-                
+class Metadata(object):
+    """
+    Metadata of an xform, from a meta block structured like:
+        
+        <Meta>
+            <clinic_id />
+            <TimeStart />
+            <TimeEnd />
+            <username />
+            <user_id />
+            <uid />
+        </Meta>
+    
+    Everything is optional.
+    """
+    """
+    clinic_id = StringProperty()
+    time_start = DateTimeProperty()
+    time_end = DateTimeProperty()
+    username = StringProperty()
+    user_id = StringProperty()
+    uid = StringProperty()
+    """
+    clinic_id = None
+    time_start = None
+    time_end = None
+    username = None
+    user_id = None
+    uid = None
+
+    def __init__(self, meta_block):
+        if const.TAG_META_CLINIC_ID in meta_block:
+            self.clinic_id = str(meta_block[const.TAG_META_CLINIC_ID])
+        if const.TAG_META_TIMESTART in meta_block:
+            self.time_start = string_to_datetime(meta_block[const.TAG_META_TIMESTART])
+        if const.TAG_META_TIMEEND in meta_block:
+            self.time_end = string_to_datetime(meta_block[const.TAG_META_TIMEEND])
+        if const.TAG_META_USERNAME in meta_block:
+            self.username = meta_block[const.TAG_META_USERNAME]
+        if const.TAG_META_USER_ID in meta_block:
+            self.user_id = meta_block[const.TAG_META_USER_ID]
+        if const.TAG_META_UID in meta_block:
+            self.uid = meta_block[const.TAG_META_UID]
+    
+    def to_dict(self):
+        return dict([(key, getattr(self, key)) for key in \
+                     ("clinic_id", "time_start", "time_end",
+                      "username", "user_id","uid")])
 
 class CXFormInstance(Document):
     """An XForms instance."""
@@ -78,7 +133,10 @@ class CXFormInstance(Document):
     @property
     def metadata(self):
         if (const.TAG_META) in self.all_properties():
-            return Metadata.from_meta_block(self.all_properties()[const.TAG_META])
+            meta_block = self.all_properties()[const.TAG_META]
+            meta = Metadata(meta_block)
+            return meta
+            
         return None
         
     class Meta:
