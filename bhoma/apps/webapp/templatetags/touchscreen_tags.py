720d5ce2d3f71df47cfb5676e87dfdc25d407b8b
Cory Zue <czue@dimagi.com>
2010-08-04 14:13:13 +0200

    remove printouts

--- bhoma/apps/webapp/templatetags/touchscreen_tags.py
+++ bhoma/apps/webapp/templatetags/touchscreen_tags.py
@@ -10,8 +10,6 @@ def ts_button(text, url, css_class="really-big-button"):
     '''
     Create a touchscreen friendly button, with some options
     '''
-    print text
-    print url
     return render_to_string("touchscreen/button.html", 
                             { "class": css_class, "text": text, "url": url })
     
