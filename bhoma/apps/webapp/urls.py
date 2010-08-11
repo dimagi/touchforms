e95e2167b8741a784039d1cdf0a79ac5becdafa7
Cory Zue <czue@dimagi.com>
2010-08-11 13:58:57 +0200

    removing login_required from urls.py.  our middleware negates the need for this

--- bhoma/apps/webapp/urls.py
+++ bhoma/apps/webapp/urls.py
@@ -4,19 +4,18 @@
 from django.conf.urls.defaults import *
 from . import views
 from django.views.generic.simple import direct_to_template
-from django.contrib.auth.decorators import login_required
 from bhoma.apps.webapp.touchscreen.options import TouchscreenOptions
 
 urlpatterns = patterns('',
-    url(r'^$', login_required(direct_to_template), {"template": "landing_page.html",
-                                                    "extra_context": {"options": TouchscreenOptions.default() }},
-                                                    name="landing_page"),
+    url(r'^$', direct_to_template, {"template": "landing_page.html",
+                                    "extra_context": {"options": TouchscreenOptions.default() }},   
+                                    name="landing_page"),
     
     url(r'^accounts/login_ts/$', views.touchscreen_login, name="touchscreen_login"),
     url(r'^accounts/logout_ts/$', views.touchscreen_logout, name="touchscreen_logout"),
     url(r'^accounts/login/$', views.bhoma_login, name="login"),
     url(r'^accounts/logout/$', views.logout, name="logout"),
-    url(r'^bhoma/admin$', login_required(direct_to_template), 
+    url(r'^bhoma/admin$', direct_to_template, 
         {"template": "admin.html",
          "extra_context": {"options": TouchscreenOptions.default() }},
          name="bhoma_admin"),
