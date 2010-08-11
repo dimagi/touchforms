9d91d9c76381e075b4f7ba79046c35a81ea12d00
Cory Zue <czue@dimagi.com>
2010-08-11 10:58:28 +0200

    add touchscreen view of report list

--- bhoma/apps/webapp/templatetags/touchscreen_tags.py
+++ bhoma/apps/webapp/templatetags/touchscreen_tags.py
@@ -6,10 +6,11 @@ from django.template.loader import render_to_string
 register = template.Library()
 
 @register.simple_tag
-def ts_button(text, url, css_class="really-big-button"):
+def ts_button(text, url, css_class="really-big-button", custom_style=""):
     '''
     Create a touchscreen friendly button, with some options
     '''
     return render_to_string("touchscreen/button.html", 
-                            { "class": css_class, "text": text, "url": url })
+                            {"class": css_class, "text": text, "url": url,
+                             "custom_style": custom_style })
     
