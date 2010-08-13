7ca44db72365cd5aefa7c6e6827ec61e41751b3f
Cory Zue <czue@dimagi.com>
2010-08-13 17:31:52 +0200

    fix bug that prevented phone submitted forms from showing up in counts

--- bhoma/apps/xforms/models/_design/views/counts_by_type/map.js
+++ bhoma/apps/xforms/models/_design/views/counts_by_type/map.js
@@ -1,6 +1,7 @@
 function(doc) { 
     // parse a date in yyyy-mm-dd format
 	function parse_date(date_string) {
+	    if (!date_string) return new Date(1970,1,1);
 	    // hat tip: http://stackoverflow.com/questions/2587345/javascript-date-parse    
 	    var parts = date_string.match(/(\d+)/g);
 	    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
@@ -8,7 +9,14 @@ function(doc) {
 	}
      
 	function get_encounter_date(xform_doc) {
-	    return parse_date(xform_doc.encounter_date);
+	    function get_date_string(xform_doc) {
+	        // check some expected places for a date
+	        if (xform_doc.encounter_date) return xform_doc.encounter_date;
+	        if (xform_doc.meta && xform_doc.meta.TimeEnd) return xform_doc.meta.TimeEnd;
+	        if (xform_doc.meta && xform_doc.meta.TimeStart) return xform_doc.meta.TimeStart;
+	        return null;
+	    }
+	    return parse_date(get_date_string(xform_doc));
 	}
     
     if (doc["#doc_type"] == "XForm") {
