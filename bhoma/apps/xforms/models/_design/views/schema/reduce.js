c76c989ffd825aab066e814ab351de3705d2c720
Daniel Roberts <droberts@dimagi.com>
2010-09-03 14:44:38 -0400

    fixed schema behavior on embedded lists

--- bhoma/apps/xforms/models/_design/views/schema/reduce.js
+++ bhoma/apps/xforms/models/_design/views/schema/reduce.js
@@ -13,15 +13,28 @@ function(key, values) {
         var kind2 = get_kind(type2);
 
         if (kind1 != kind2) {
-            if(kind1 == "null" || kind2 == "null") {
-                if(kind1 == "null") {
+            if(kind2 == "null") {
+            /* if someone calls reconcile(type, null),
+            then swap it to be reconcile(null, type) */
+                var tmp = kind2;
+                kind2 = kind1;
+                kind1 = tmp;
+                tmp = type2;
+                type2 = type1;
+                type1 = tmp;
+            }
+            if(kind1 == "null") {
+                if(kind2 == "list") {
+                    type = [reconcile_list(type2)];
+                }
+                else if(kind2 == 'dict') {
+                    type = {};
+                    for (var key in type2) {
+                        type[key] = reconcile(null, type2[key]);
+                    }
+                }
+                else {
                     type = type2;
-                }
-                else if (kind2 == "null") {
-                    type = type1;
-                }
-                if(get_kind(type) == "list") {
-                    type = [reconcile_list(type)];
                 }
             }
             else if (kind1 == 'list') {
@@ -38,7 +51,13 @@ function(key, values) {
         else {
             if(kind1 == 'dict') {
                 type = {};
+                for(var key in type1) {
+                    type[key] = null;
+                }
                 for(var key in type2) {
+                    type[key] = null;
+                }
+                for(var key in type) {
                     type[key] = reconcile(type1[key], type2[key]);
                 }
             }
@@ -49,7 +68,7 @@ function(key, values) {
                 type = type1;
             }
         }
-        if (depth > 5) {
+        if (depth > 10) {
             log(depth);
             log(kind1 + ":\n\t" + uneval(type1));
             log(kind2 + ":\n\t" + uneval(type2));
