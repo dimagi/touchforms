cd7e2364f70d8145411bb81e441778115fff629e
Drew Roos <droos@dimagi.com>
2010-12-08 18:26:48 -0500

    enhance couch sync analysis script to be aware of deleted records

--- bhoma/apps/webapp/management/commands/couchinventory.py
+++ bhoma/apps/webapp/management/commands/couchinventory.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         db = get_db()
 
-        stream = (len(args) > 0 and args[-1] == '-')
+        stream = ('-' in args)
         if stream:
             outpath = 'stdout'
             fout = sys.stdout
@@ -21,19 +21,39 @@ class Command(BaseCommand):
             outpath = OUTFILE % {'timestamp': datetime.now().strftime('%Y%m%d%H%M%S')}
             fout = open(outpath, 'w')
 
-        rows = db.get('_all_docs')['rows']
+        incl_deleted = ('x' in args)
 
+        docids = set()
+        rows = db.get('_all_docs')['rows']
         for doc in rows:
             docid = doc['id']
             if docid.startswith('_design'):
                 continue
             rev = doc['value']['rev']
 
+            docids.add(docid)
             fout.write(hashify(docid, DOCID_HASH_LEN))
             fout.write(hashify(rev, REV_HASH_LEN))
 
-        sys.stderr.write('dump written to %s; %d records; %.1fKB\n' % \
-            (outpath, len(rows), (DOCID_HASH_LEN + REV_HASH_LEN) * len(rows) / 1024.))
+        deleted_docs = set()
+        if incl_deleted:
+            rows = db.get('_changes')['results']
+            for tx in rows:
+                docid = tx['id']
+                if docid.startswith('_design'):
+                    continue
+                if not ('deleted' in tx and tx['deleted']):
+                    continue
+                if docid in docids:
+                    continue
+
+                docids.add(docid)
+                deleted_docs.add(docid)
+                fout.write(hashify(docid, DOCID_HASH_LEN))
+                fout.write('\xff' * REV_HASH_LEN)
+
+        sys.stderr.write('dump written to %s; %d records%s; %.1fKB\n' % \
+            (outpath, len(docids), ' (%d deleted)' % len(deleted_docs) if incl_deleted else '', (DOCID_HASH_LEN + REV_HASH_LEN) * len(docids) / 1024.))
 
 def hashify(data, hashlen):
     return hashlib.sha1(data).digest()[:hashlen]
