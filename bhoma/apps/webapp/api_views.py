3073b3483a9e4807a2b2a7f9ee80d65ae96db91c
Drew Roos <droos@dimagi.com>
2011-01-06 23:59:16 -0500

    add server-side caching for autocomplete lookups

--- bhoma/apps/webapp/api_views.py
+++ bhoma/apps/webapp/api_views.py
@@ -67,6 +67,8 @@ def phone_home(request, tag):
 
 #TEMPORARY
 
+from django.core.cache import cache
+
 DATASET = 'us'
 #DATASET = 'zam'
 
@@ -76,7 +78,7 @@ def load_census_file(path):
         for ln in lines:
             name = ln[:15].strip()
             prob = float(ln[15:20]) / 100.
-            yield {'name': name, 'p': 2e5 * prob}
+            yield {'name': name, 'p': max(2e5 * prob, 1.)}
 
 def load_raw_file(path):
     with open(path) as f:
@@ -87,40 +89,7 @@ def load_raw_file(path):
             prob = int(pcs[1].strip())
             yield {'name': name, 'p': prob}
 
-def lookup_names(domain, key, maxnum):
-    if DATASET == 'zam':
-        path = '/home/drew/tmp/census/' + {
-            'firstname-male': 'zamnamesfirstmale',
-            'firstname-female': 'zamnamesfirstfemale',
-            'lastname': 'zamnameslast'
-            }[domain]
-        loadfunc = load_raw_file
-    else:
-        path = '/home/drew/tmp/census/' + {
-            'firstname-male': 'dist.male.first',
-            'firstname-female': 'dist.female.first',
-            'lastname': 'dist.all.last'
-            }[domain]
-        loadfunc = load_census_file
- 
-    return get_matches(loadfunc(path), key, maxnum)
-
-def lookup_villages(domain, key, maxnum):
-    if DATASET == 'zam':
-        path = '/home/drew/tmp/census/zamvillage'
-    else:
-        path = '/home/drew/tmp/census/usplaces'
-
-    villages = list(load_raw_file(path))
-
-    if DATASET == 'zam':
-        villages.sort(key=lambda v: -v['p'])
-
-    return get_matches(villages, key, maxnum)
-
 def get_matches(data, key, maxnum, matchfunc=None):
-    data = list(data)
-
     if matchfunc == None:
         matchfunc = lambda key, name: name.startswith(key)
 
@@ -150,19 +119,115 @@ def autocomplete(request):
     key = request.GET.get('key', '')
     max_results = int(request.GET.get('max', '20'))
 
-    if domain == 'village':
-        matches = lookup_villages(domain, key, max_results)
-    elif domain == 'firstname':
-        matchesm = lookup_names('firstname-male', key, max_results)
-        matchesf = lookup_names('firstname-female', key, max_results)
-        matches = []
-        matches.extend(matchesm)
-        matches.extend(matchesf)
-        matches.sort(key=lambda m: -m['p'])
-        matches = matches[:max_results]
+    if domain != 'firstname':
+        response = get_suggestions(domain, key, max_results)
     else:
-        matches = lookup_names(domain, key, max_results)
+        resp_m = get_suggestions('firstname-male', key, max_results)
+        resp_f = get_suggestions('firstname-female', key, max_results)
+        response = {}
+        response['suggestions'] = []
+        response['suggestions'].extend(resp_m['suggestions'])
+        response['suggestions'].extend(resp_f['suggestions'])
+        response['suggestions'].sort(key=lambda m: -m['p'])
+        response['suggestions'] = response['suggestions'][:max_results]
+        response['hinting'] = resp_m['hinting']
+        response['hinting']['sample_size'] += resp_f['hinting']['sample_size']
+        for c, k in resp_f['hinting']['nextchar_freq'].iteritems():
+            if c not in response['hinting']['nextchar_freq']:
+                response['hinting']['nextchar_freq'][c] = 0
+            response['hinting']['nextchar_freq'][c] += k
 
-    return HttpResponse(json.dumps(matches), 'text/json')
+    return HttpResponse(json.dumps(response), 'text/json')
 
+def get_suggestions(domain, key, maxnum):
+    if not cacheget((domain, 'initialized')):
+        init_cache(domain)
 
+    response = cacheget((domain, 'results', key))
+    if not response:
+        print 'no response cached', (domain, 'results', key)
+        rawdata = None
+        lookup_key = key
+        while rawdata == None and len(lookup_key) > 0:
+            rawdata = cacheget((domain, 'raw', lookup_key))
+            lookup_key = lookup_key[:-1]
+        if rawdata == None:
+            #prefix for which no matches exist
+            rawdata = []
+        response = get_response(domain, key, rawdata)
+    response['suggestions'] = response['suggestions'][:maxnum]
+    return response
+
+def group(it, func):
+    grouped = {}
+    for e in it:
+        key = func(e)
+        if key not in grouped:
+            grouped[key] = []
+        grouped[key].append(e)
+    return grouped
+
+def init_cache(domain):
+    IX_LEN = 3
+
+    data = load_data(domain)
+    for i in range(IX_LEN + 1):
+        subdata = group(data, lambda e: e['name'][:i])
+        for key, records in subdata.iteritems():
+            if len(key) != i:
+                continue
+            print i, key
+            cacheset((domain, 'results', key), get_matches(records, key, 20))
+            if i == IX_LEN:
+                cacheset((domain, 'raw', key), records)
+    cacheset((domain, 'initialized'), True)
+
+def get_response(domain, key, data):
+    response = get_matches(data, key, 20)
+    cacheset((domain, 'results', key), response)
+    return response
+
+def load_data(domain):
+    if domain == 'village':
+        if DATASET == 'zam':
+            path = '/home/drew/tmp/census/zamvillage'
+        else:
+            path = '/home/drew/tmp/census/usplaces'
+        data = load_raw_file(path)
+    else:
+        if DATASET == 'zam':
+            path = '/home/drew/tmp/census/' + {
+                'firstname-male': 'zamnamesfirstmale',
+                'firstname-female': 'zamnamesfirstfemale',
+                'lastname': 'zamnameslast'
+                }[domain]
+            loadfunc = load_raw_file
+        else:
+            path = '/home/drew/tmp/census/' + {
+                'firstname-male': 'dist.male.first',
+                'firstname-female': 'dist.female.first',
+                'lastname': 'dist.all.last'
+                }[domain]
+            loadfunc = load_census_file
+        data = loadfunc(path)
+    data = list(data)
+    data.sort(key=lambda v: -v['p'])
+    return data
+
+import base64
+
+def enc(data):
+    return base64.b64encode(json.dumps(data))
+
+def dec(data):
+    return json.loads(base64.b64decode(data))
+
+def cacheget(key):
+    data = cache.get(enc(key))
+    if data == None:
+        return None
+    else:
+        return dec(data)
+
+def cacheset(key, val):
+    cache.set(enc(key), enc(val), 3600)
