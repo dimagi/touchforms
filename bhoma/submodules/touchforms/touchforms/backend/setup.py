a8616c77664369ec7fe87bacfff8ff326233f85f
Drew Roos <droos@dimagi.com>
2011-02-03 15:54:56 -0500

    start moving touchforms into subrepo

--- bhoma/submodules/touchforms/touchforms/backend/setup.py
+++ bhoma/submodules/touchforms/touchforms/backend/setup.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-BHOMA_BASE = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CUR_DIR = os.path.dirname(__file__)
 
 def init_classpath():
     for jar in ('javarosa-libraries.jar', 'kxml2-2.3.0.jar', 'regexp-me.jar'):
         if jar not in sys.path:
-            sys.path.append(os.path.join(BHOMA_BASE, "formentry", "jrlib", jar))
+            sys.path.append(os.path.join(CUR_DIR, "jrlib", jar))
 
