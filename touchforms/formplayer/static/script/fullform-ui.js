108c72e25215000e79b74ae7477351c4366fb7a4
Drew Roos <droos@dimagi.com>
2011-10-21 19:40:46 -0400

    dynamic show/hide for relevancy

--- touchforms/formplayer/static/script/fullform-ui.js
+++ touchforms/formplayer/static/script/fullform-ui.js
@@ -58,6 +58,11 @@ function Group(json, parent) {
       });
   }
 
+  this.reconcile = function(new_json) {
+    //reconcile local fields
+    reconcile_elements(this, new_json.children);    
+  }
+
   this.child_container = function() {
     return this.$children;
   }
@@ -83,6 +88,11 @@ function Repeat(json, parent) {
       });
   }
 
+  this.reconcile = function(new_json) {
+    //reconcile local fields
+    reconcile_elements(this, new_json.children);    
+  }
+
   this.child_container = function() {
     return this.$children;
   }
@@ -105,6 +115,7 @@ function Question(json, parent) {
   }
 
   this.reconcile = function(new_json) {
+    console.log('imma reconcilin\' ' + this.ix);
     //update caption only?
     //compare everything, i guess
     //note that select choices may change due to itemsets
@@ -123,9 +134,7 @@ function Question(json, parent) {
   }
 }
 
-function render_elements(parent, elems) {
-  for (var i = 0; i < elems.length; i++) {
-    var e = elems[i];
+function make_element(e, parent) {
     if (e.type == 'question') {
       var o = new Question(e, parent);
     } else if (e.type == 'sub-group') {
@@ -134,13 +143,76 @@ function render_elements(parent, elems) {
       var o = new Repeat(e, parent);
     }
     o.init_render();
+    return o;
+}
+
+function render_elements(parent, elems) {
+  for (var i = 0; i < elems.length; i++) {
+    var o = make_element(elems[i], parent);
     parent.children.push(o);
     parent.child_container().append(o.$container);
   }
 }
 
 function reconcile_elements(parent, new_elems) {
+  var mapping = []; // (k_old, k_new)
 
+  var inElementSet = function(e, set) {
+    return $.map(set, function(val) { return val.ix; }).indexOf(e.ix);
+    //compare repeat uuids here?
+  }
+
+  for (var i = 0; i < parent.children.length; i++) {
+    var child = parent.children[i];
+    mapping.push([i, inElementSet(child, new_elems)]);
+  }
+  for (var i = 0; i < new_elems.length; i++) {
+    if (inElementSet(new_elems[i], parent.children) == -1) {
+      mapping.push([-1, i]);
+    }
+  }
+  // sort by k_new so that inserts are processed in the right order
+  mapping.sort(function(a, b) {
+      return a[1] - b[1];
+    });
+
+  $.each(mapping, function(i, val) {
+      var k_old = val[0];
+      var k_new = val[1];
+
+      if (k_old == -1) {
+        var o = make_element(new_elems[k_new]);
+        addChild(parent, k_new, o);
+      } else if (k_new == -1) {
+        deleteChild(parent, k_old);
+      } else {
+        parent.children[k_old].reconcile(new_elems[k_new]);
+      }
+    });
+}
+
+function deleteChild(parent, i) {
+  var child = arrayDel(parent.children, i);
+  child.$container.remove();
+  //todo: fix focus
+  //todo: probably better to enqueue DOM elems to be deleted, and process in bulk
+}
+
+function addChild(parent, i, child) {
+  if (i < parent.children.length) {
+    parent.children[i].$container.before(child.$container);
+  } else {
+    parent.$container.append(child.$container);
+  }
+  arrayInsertAt(parent.children, i, child);
+}
+
+function arrayDel(arr, i) {
+  return arr.splice(i, 1)[0];
+}
+
+function arrayInsertAt(arr, i, o) {
+  arr.splice(i, 0, o);
 }
 
 function tmprender(elems) {
