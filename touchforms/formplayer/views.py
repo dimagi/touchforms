0785ab05d05aac4f4a962a8a0ae42e8f5a3f9e4b
dmyung <dmyung@dimagi.com>
2011-12-09 15:17:38 -0500

    adding better json handling for preloader data.
    issue was that if and when preloader data would contain a datetime() parsed from couchdbkit,
    it'd fail on a json.dumps() unless the document pulled out of couch would call to_json() explicitly.

--- touchforms/formplayer/views.py
+++ touchforms/formplayer/views.py
@@ -107,7 +107,6 @@ def form_entry_new(request, xform, instance_xml, preloader_data, input_mode,
             'type': 'typeforms.html',
             'full': 'fullform.html',
         }[input_mode]
-
     return render_to_response(templ, {
             "form": xform,
             "mode": 'xform',
@@ -204,7 +203,7 @@ def play_remote(request, session_id=None, playsettings=None):
         return HttpResponseRedirect(session.abort if session.abort else session.next)
     return enter_form(request, 
                       xform_id=session.xform_id,
-                      preloader_data=session.preloader_data,
+                      preloader_data=session.to_json()['preloader_data'],
                       input_mode=session.input_mode,
                       onsubmit=onsubmit,
                       onabort=onabort,
