23727ffb4a7a474f5392e623b2de13b0dc63b42b
Drew Roos <droos@dimagi.com>
2012-05-15 16:33:10 -0400

    add utility javascript to prevent divs from scrolling off-screen

--- touchforms/formplayer/static/formplayer/script/fullform-ui.js
+++ touchforms/formplayer/static/formplayer/script/fullform-ui.js
@@ -521,3 +521,23 @@ var answer_eq = function(ans1, ans2) {
   return false;
 }
 
+function scroll_pin(pin_threshold, $container, $elem) {
+  var base_offset = $container.offset().top;
+  var pos_type = $elem.css('position');
+
+  return function() {
+    var scroll_pos = $(window).scrollTop();
+    var elem_pos = base_offset - scroll_pos;
+    var pinned = (elem_pos < pin_threshold);
+
+    $elem.css('position', pinned ? 'fixed' : pos_type);
+    $elem.css('top', pinned ? pin_threshold + 'px' : 0);
+  };
+}
+  
+function set_pin(pin_threshold, $container, $elem) {
+  var pinfunc = scroll_pin(pin_threshold, $container, $elem);
+  $(window).scroll(pinfunc);
+  pinfunc();
+}
+
