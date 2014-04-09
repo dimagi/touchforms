85727f49f34bb794ed4e55521a351e67eba66c32
Cory Zue <czue@dimagi.com>
2014-04-09 12:49:34 +0000

    better error handling when things go wrong starting a session
    http://manage.dimagi.com/default.asp?107561

--- touchforms/formplayer/views.py
+++ touchforms/formplayer/views.py
@@ -209,7 +209,7 @@ def player_proxy(request):
 
 def track_session(request, payload, response):
     action = payload['action']
-    if action == 'new-form' and 'form-url' in payload:
+    if action == 'new-form' and 'form-url' in payload and 'session_id' in response:
         session_id = response['session_id']
         session_name = payload['session-data'].get(
             'session_name', response.get('title', _('Unknown Form'))
