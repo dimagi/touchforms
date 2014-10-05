e6b0a0ca13a3520c17228f96be68c231894a1687
NoahCarnahan <noah.carnahan@gmail.com>
2014-10-05 15:38:32 +0300

    Made TouchFormsException __unicode__ and __str__ methods more robust

--- touchforms/backend/xcp.py
+++ touchforms/backend/xcp.py
@@ -1,9 +1,17 @@
 class TouchFormsException(Exception):
     def __unicode__(self):
         if len(self.args) == 1:
-            return unicode(self.args[0])
+            return unicode(self.args[0], errors="replace")
         else:
             return unicode(self.args)
+
+    def __str__(self):
+        if len(self.args) == 1:
+            if isinstance(self.args[0], unicode):
+                return self.args[0].encode("ascii", "xmlcharrefreplace")
+            return str(self.args[0])
+        return repr(self.args)
+
 
 class EmptyCacheFileException(TouchFormsException):
     pass
