192f961a5aaed5a6648c45a60df8905d6e76c589
Ben <rudolphben@gmail.com>
2015-04-27 11:03:54 -0400

    Fix indentation

--- touchforms/Gruntfile.js
+++ touchforms/Gruntfile.js
@@ -2,20 +2,20 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jst: {
-        compile: {
-            files: {
-                'formplayer/static/formplayer/script/fullform-ui.templates.js': [
-                    'formplayer/templates/formplayer/fullform-ui/*.html'
-                ]
+        pkg: grunt.file.readJSON('package.json'),
+        jst: {
+            compile: {
+                files: {
+                    'formplayer/static/formplayer/script/fullform-ui.templates.js': [
+                        'formplayer/templates/formplayer/fullform-ui/*.html'
+                    ]
+                }
             }
+        },
+        watch: {
+            files: 'formplayer/templates/formplayer/fullform-ui/*.html',
+            tasks: ['jst']
         }
-    },
-    watch: {
-        files: 'formplayer/templates/formplayer/fullform-ui/*.html',
-        tasks: ['jst']
-    }
     });
 
     // Load the plugin that provides the "jst" task.
