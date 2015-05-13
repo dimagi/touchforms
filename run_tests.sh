78ae645a681449c73140707b3945ab7b076136b2
Ben <rudolphben@gmail.com>
2015-05-12 20:40:48 -0400

    Fix spelling mistake

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-cd touchforms && jasmine-ci --browser phantomjs && jython backend/test_server.py && jython backend/test_formplayer.py
+cd touchforms && jasmine-ci --browser phantomjs && jython backend/test_server.py && jython backend/test_xformplayer.py
