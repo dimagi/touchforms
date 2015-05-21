f033a65df41ec8d642d89620eb9a2fbe516fe14a
Ben <rudolphben@gmail.com>
2015-05-20 23:00:36 -0400

    Update .py in tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-cd touchforms && jasmine-ci --browser phantomjs && jython backend/test_server.py && jython backend/test_xformplayer.py && jython backend/test_global_state_manager
+cd touchforms && jasmine-ci --browser phantomjs && jython backend/test_server.py && jython backend/test_xformplayer.py && jython backend/test_global_state_manager.py
