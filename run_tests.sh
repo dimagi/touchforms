de2c8c3af4647414fb532be07b509ae38f182735
Cory Zue <czue@dimagi.com>
2015-06-05 18:37:37 +0200

    add hashbin thing

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 cd touchforms && jasmine-ci --browser phantomjs && jython backend/test_server.py && jython backend/test_xformplayer.py && jython backend/test_touchcare.py && jython backend/test_global_state_manager.py
