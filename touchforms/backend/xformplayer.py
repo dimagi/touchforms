50bd2f148ee808c4f6df90c64ac3a99252b9a6d2
Ben <brudolph@dimagi.com>
2016-04-13 18:32:40 -0400

    Remove unused process_form_xml

--- touchforms/backend/xformplayer.py
+++ touchforms/backend/xformplayer.py
@@ -725,14 +725,6 @@ def submit_form(xform_session, answers, prevalidated):
     else:
         resp = form_completion(xform_session)
         resp['status'] = 'success'
-        xml = xform_session.output()
-        if xform_session.uses_sql_backend:
-            process_form_xml(
-                xform_session.orig_params['api_auth'],
-                xml,
-                xform_session.orig_params['session_data'],
-            )
-
     return xform_session.response(resp, no_next=True)
 
 
