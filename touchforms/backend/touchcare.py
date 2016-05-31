eee7926e5c41cbba8951d9132b6e81acf0743673
wpride <wpride@dimagi.com>
2016-05-31 10:29:20 -0400

    Say "lookup table" instead of "fixture"
    http://manage.dimagi.com/default.asp?228203

--- touchforms/backend/touchcare.py
+++ touchforms/backend/touchcare.py
@@ -187,8 +187,8 @@ class CCInstances(CommCareInstanceInitializer):
                 raise TouchFormsNotFound('This form requires that the user be assigned to a location '
                                          'but one could not be found.')
             else:
-                raise TouchFormsNotFound('Unable to fetch fixture %s. '
-                                         'Ensure the logged in user has access to this fixture.' % fixture_name)
+                raise TouchFormsNotFound('Unable to fetch lookup table %s. '
+                                         'Ensure the logged in user has access to this lookup table.' % fixture_name)
         parser = KXmlParser()
         parser.setInput(to_input_stream(results), "UTF-8")
         parser.setFeature(KXmlParser.FEATURE_PROCESS_NAMESPACES, True)
