6d5758bdaff245075146960a53d92172320af7d9
Ben <brudolph@dimagi.com>
2016-08-04 18:45:09 -0400

    Allow minus and periods

--- touchforms/spec/javascripts/entrycontrols_full_spec.js
+++ touchforms/spec/javascripts/entrycontrols_full_spec.js
@@ -200,4 +200,13 @@ describe('Entries', function() {
         entry.rawAnswer('')
         expect(entry.answer()).toBe(Formplayer.Const.NO_ANSWER);
     });
+
+    it('Should allow decimals in a PhoneEntry', function() {
+        questionJSON.datatype = Formplayer.Const.STRING;
+        questionJSON.style = { raw: 'numeric' };
+
+        entry = (new Question(questionJSON)).entry;
+        entry.rawAnswer('123.4');
+        expect(entry.answer()).toBe('123.4');
+    });
 });
