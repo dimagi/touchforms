786e301794e011df88ddc30e7ce356a407f9b470
Will Pride <wpride@dimagi.com>
2018-02-13 12:21:32 -0500

    assert -> assertTrue/False

--- touchforms/formplayer/tests/test_experiment.py
+++ touchforms/formplayer/tests/test_experiment.py
@@ -9,36 +9,36 @@ class ExperimentTest(SimpleTestCase):
 
     def testExperimentStringCompare(self):
         # session_id will never be equal, should be ignored
-        assert formplayer_string_compare("123", "234", current_key="session_id")
+        self.assertTrue(formplayer_string_compare("123", "234", current_key="session_id"))
         # same with output XML (for the time being)
-        assert formplayer_string_compare("123", "234", current_key="output")
+        self.assertTrue(formplayer_string_compare("123", "234", current_key="output"))
 
-        assert formplayer_string_compare(0, "false", current_key="repeatable")
-        assert formplayer_string_compare(1, "true", current_key="repeatable")
-        assert not formplayer_string_compare(1, "false", current_key="repeatable")
-        assert not formplayer_string_compare(0, "true", current_key="repeatable")
+        self.assertTrue(formplayer_string_compare(0, "false", current_key="repeatable"))
+        self.assertTrue(formplayer_string_compare(1, "true", current_key="repeatable"))
+        self.assertTrue(not formplayer_string_compare(1, "false", current_key="repeatable"))
+        self.assertTrue(formplayer_string_compare(0, "true", current_key="repeatable"))
 
     def testExperimentDictCompare(self):
 
         self.reset()
 
-        assert formplayer_compare(self.control, self.candidate)
+        self.assertTrue(formplayer_compare(self.control, self.candidate))
 
         self.candidate['random_key'] = 'wrong_value'
-        assert not formplayer_compare(self.control, self.candidate)
+        self.assertFalse(formplayer_compare(self.control, self.candidate))
         self.reset()
 
         self.candidate['repeatable'] = 'true'
-        assert not formplayer_compare(self.control, self.candidate)
+        self.assertFalse(formplayer_compare(self.control, self.candidate))
         self.reset()
 
         # should fail if control has a key that the candidate does not
         self.control['extra_key'] = 'extra_value'
-        assert not formplayer_compare(self.control, self.candidate)
+        self.assertFalse(formplayer_compare(self.control, self.candidate))
         self.reset()
         # allow candidate to have extra keys (Yes?)
         self.candidate['extra_key'] = 'extra_key'
-        assert formplayer_compare(self.control, self.candidate)
+        self.assertFalse(formplayer_compare(self.control, self.candidate))
         self.reset()
 
     def reset(self):
