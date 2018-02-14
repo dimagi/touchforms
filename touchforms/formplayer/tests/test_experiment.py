22465c998d26a0d1f317ab46ccbcaf3ad49902dc
Will Pride <wpride@dimagi.com>
2018-02-14 09:12:36 -0500

    fix tests (hopefully)

--- touchforms/formplayer/tests/test_experiment.py
+++ touchforms/formplayer/tests/test_experiment.py
@@ -1,54 +1,56 @@
 from django.test import SimpleTestCase
 from touchforms.formplayer.experiments import formplayer_string_compare, formplayer_compare
 
-
 class ExperimentTest(SimpleTestCase):
 
-    control = {}
-    candidate = {}
-
     def setup(self):
-        self.reset()
+        self.resetDicts()
 
     def testExperimentStringCompare(self):
         # session_id will never be equal, should be ignored
-        self.assertTrue(formplayer_string_compare("123", "234", current_key="session_id"))
+        self.assertTrue(formplayer_string_compare("123", "234", key="session_id"))
         # same with output XML (for the time being)
-        self.assertTrue(formplayer_string_compare("123", "234", current_key="output"))
+        self.assertTrue(formplayer_string_compare("123", "234", key="output"))
 
-        self.assertTrue(formplayer_string_compare(0, "false", current_key="repeatable"))
-        self.assertTrue(formplayer_string_compare(1, "true", current_key="repeatable"))
-        self.assertTrue(not formplayer_string_compare(1, "false", current_key="repeatable"))
-        self.assertTrue(formplayer_string_compare(0, "true", current_key="repeatable"))
+        self.assertTrue(formplayer_string_compare(0, "false", key="repeatable"))
+        self.assertTrue(formplayer_string_compare(1, "true", key="repeatable"))
+        self.assertFalse(formplayer_string_compare(1, "false", key="repeatable"))
+        self.assertFalse(formplayer_string_compare(0, "true", key="repeatable"))
 
     def testExperimentDictCompare(self):
-        self.assertTrue(formplayer_compare(self.control, self.candidate))
+        (control, candidate) = self.resetDicts()
+        self.assertTrue(formplayer_compare(control, candidate))
+        candidate['random_key'] = 'wrong_value'
+        self.assertFalse(formplayer_compare(control, candidate))
 
-        self.candidate['random_key'] = 'wrong_value'
-        self.assertFalse(formplayer_compare(self.control, self.candidate))
-        self.reset()
+        (control, candidate) = self.resetDicts()
 
-        self.candidate['repeatable'] = 'true'
-        self.assertFalse(formplayer_compare(self.control, self.candidate))
-        self.reset()
+        candidate['repeatable'] = 'true'
+        self.assertFalse(formplayer_compare(control, candidate))
 
+        (control, candidate) = self.resetDicts()
         # should fail if control has a key that the candidate does not
-        self.control['extra_key'] = 'extra_value'
-        self.assertFalse(formplayer_compare(self.control, self.candidate))
-        self.reset()
-        # allow candidate to have extra keys (Yes?)
-        self.candidate['extra_key'] = 'extra_key'
-        self.assertFalse(formplayer_compare(self.control, self.candidate))
-        self.reset()
+        control['extra_key'] = 'extra_value'
+        self.assertFalse(formplayer_compare(control, candidate))
 
-    def reset(self):
-        self.control = {
+        (control, candidate) = self.resetDicts()
+        # allow candidate to have extra keys (Yes?)
+        candidate['extra_key'] = 'extra_key'
+        self.assertTrue(formplayer_compare(control, candidate))
+
+        (control, candidate) = self.resetDicts()
+        candidate = candidate.pop('session_id')
+        self.assertFalse(formplayer_compare(control, candidate))
+
+    def resetDicts(self):
+        control = {
             'session_id': 'session_id_control',
             'random_key': 'right_value',
             'repeatable': 0
         }
-        self.candidate = {
+        candidate = {
             'session_id': 'session_id_candidate',
             'random_key': 'right_value',
             'repeatable': 'false'
         }
+        return (control, candidate)
